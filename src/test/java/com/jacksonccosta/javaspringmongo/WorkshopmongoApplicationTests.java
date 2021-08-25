6936c8496403cb7358cad4c5a4f505165a289a86
Jackson Costa <jacksonccosta@yahoo.com>
2021-08-24 21:32:55 -0300

    Projeto criado

---
+++ src/test/java/com/jacksonccosta/javaspringmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonccosta.javaspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
